bb3c51a1cec92f5bb6567394dd77c447e6d4934d
theivorysparkstudio-ship-it <theivoryspark.studio@gmail.com>
2026-01-23 00:17:10 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -50,7 +50,7 @@ const Footer = () => {
     target="_blank"
     rel="noopener noreferrer"
   >
-    <span className="w-10 h-10 flex items-center justify-center hover:bg-gold transition-all">
+    <span className="w-10 h-10 flex items-center justify-center transition-all">
       <Instagram size={20} />
     </span>
 
